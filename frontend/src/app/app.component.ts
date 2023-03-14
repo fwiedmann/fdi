c044e0d95a9a0174b3de8e684980ee38484893a1
fwiedmann <privat@felixwiedmann.de>
2023-03-14 21:14:07 +0100

    chore(fe): adjust fee

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,30 +1,26 @@
-import {Component} from '@angular/core';
-import {CrewSelectorResponse} from './crew-selections/crew-selector/crew-selector.component';
-import {MetaData} from './emergency-base-data/emergency-base-data.component';
-import {PdfGeneratorService} from './pdf-generator.service';
-import {MatDialog} from '@angular/material/dialog';
-import {CrewManagerComponent} from './crew-manager/crew-manager.component';
-
+import { Component } from '@angular/core';
+import { CrewSelectorResponse } from './crew-selections/crew-selector/crew-selector.component';
+import { MetaData } from './emergency-base-data/emergency-base-data.component';
+import { PdfGeneratorService } from './pdf-generator.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CrewManagerComponent } from './crew-manager/crew-manager.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent {
   title = 'FDI';
 
-  crewSelections: CrewSelectorResponse[] = []
+  crewSelections: CrewSelectorResponse[] = [];
   emergencyMetaData: MetaData | undefined;
   fileName: string = 'kostenaufstellung';
 
   constructor(
     private pdfGeneratorService: PdfGeneratorService,
     public dialog: MatDialog
-  ) {
-
-  }
+  ) { }
 
   generatePdf() {
     if (!this.emergencyMetaData) {
@@ -32,13 +28,18 @@ export class AppComponent {
       return;
     }
 
-    this.pdfGeneratorService.generatePdf(this.emergencyMetaData, this.crewSelections, {
-      forAHalfHourInEuro: 6,
-      forDirtAllowanceInEuro: 2
-    }, this.fileName);
+    this.pdfGeneratorService.generatePdf(
+      this.emergencyMetaData,
+      this.crewSelections,
+      {
+        forAHalfHourInEuro: 6.5,
+        forDirtAllowanceInEuro: 0,
+      },
+      this.fileName
+    );
   }
 
   openCrewMemberConfigDialog() {
-    this.dialog.open(CrewManagerComponent)
+    this.dialog.open(CrewManagerComponent);
   }
 }
--- frontend/src/app/pdf-generator.service.ts
+++ frontend/src/app/pdf-generator.service.ts
@@ -1,45 +1,48 @@
-import {Injectable} from '@angular/core';
-import {jsPDF} from 'jspdf';
-import {MetaData} from './emergency-base-data/emergency-base-data.component';
-import {CheckedMember, CrewSelectorResponse} from './crew-selections/crew-selector/crew-selector.component';
-
+import { Injectable } from '@angular/core';
+import { jsPDF } from 'jspdf';
+import { MetaData } from './emergency-base-data/emergency-base-data.component';
+import {
+  CheckedMember,
+  CrewSelectorResponse,
+} from './crew-selections/crew-selector/crew-selector.component';
 
 export type CostRate = {
   forAHalfHourInEuro: number;
   forDirtAllowanceInEuro: number;
-}
+};
 
-type pageState = { count: number, page: number }
-const defaultYStartPosition: number = 10
+type pageState = { count: number; page: number };
+const defaultYStartPosition: number = 10;
 const defaultPageState: pageState = {
   page: 1,
-  count: 0
-}
+  count: 0,
+};
 
 @Injectable({
-  providedIn: 'root'
-
+  providedIn: 'root',
 })
-
 export class PdfGeneratorService {
-
-  private doc: jsPDF
+  private doc: jsPDF;
   private pdfY = defaultYStartPosition;
   private pageState: pageState = {
     page: 1,
-    count: 0
-  }
+    count: 0,
+  };
 
   constructor() {
-    this.doc = new jsPDF()
+    this.doc = new jsPDF();
   }
 
   private static buildFullDate(date: Date): string {
     return `${date.getDate()}.${date.getUTCMonth() + 1}.${date.getFullYear()}`;
   }
 
-  private static calculateHalfHoursCount(startDate: Date, endDate: Date): number {
-    const fits = PdfGeneratorService.calculateMinutesBetweenDates(startDate, endDate) / 30;
+  private static calculateHalfHoursCount(
+    startDate: Date,
+    endDate: Date
+  ): number {
+    const fits =
+      PdfGeneratorService.calculateMinutesBetweenDates(startDate, endDate) / 30;
     fits.toFixed();
     if (!fits.toString().includes('.')) {
       return fits;
@@ -47,25 +50,33 @@ export class PdfGeneratorService {
     return Number(fits.toString().split('.')[0]) + 1;
   }
 
-  private static calculateMinutesBetweenDates(startDate: Date, endDate: Date): number {
+  private static calculateMinutesBetweenDates(
+    startDate: Date,
+    endDate: Date
+  ): number {
     const millSec = endDate.getTime() - startDate.getTime();
     return Math.floor(millSec / 60000);
   }
 
-  private static calculateCrwMemberCostRate(member: CheckedMember, costRate: CostRate, halfHourCount: number): number {
+  private static calculateCrwMemberCostRate(
+    member: CheckedMember,
+    costRate: CostRate,
+    halfHourCount: number
+  ): number {
     if (member.dirtAllowance) {
-      return (costRate.forAHalfHourInEuro * halfHourCount) + costRate.forDirtAllowanceInEuro;
+      return (
+        costRate.forAHalfHourInEuro * halfHourCount +
+        costRate.forDirtAllowanceInEuro
+      );
     }
     return costRate.forAHalfHourInEuro * halfHourCount;
   }
 
   private static buildHoursRepresentation(date: Date): string {
-
     let hour = date.getHours().toString();
     if (date.getHours() < 10) {
       hour = '0' + hour;
     }
-
 
     let minutes = date.getMinutes().toString();
     if (date.getMinutes() < 10) {
@@ -74,10 +85,15 @@ export class PdfGeneratorService {
     return hour + ':' + minutes + ' Uhr';
   }
 
-  generatePdf(metadata: MetaData, crewsSelections: CrewSelectorResponse[], costRate: CostRate, fileName: string) {
+  generatePdf(
+    metadata: MetaData,
+    crewsSelections: CrewSelectorResponse[],
+    costRate: CostRate,
+    fileName: string
+  ) {
     this.pdfY = 10;
     this.doc = new jsPDF();
-    this.pageState = defaultPageState
+    this.pageState = defaultPageState;
 
     // header
     this.doc.setFont('helvetica', 'normal');
@@ -97,102 +113,148 @@ export class PdfGeneratorService {
     this.addEmptyLine();
 
     // Emergency meta data
-    this.doc.text(`Anlage zum Einsatzbericht der FF Schutterwald vom ${PdfGeneratorService.buildFullDate(metadata.startDate)}`, 20, this.addY(5));
+    this.doc.text(
+      `Anlage zum Einsatzbericht der FF Schutterwald vom ${PdfGeneratorService.buildFullDate(
+        metadata.startDate
+      )}`,
+      20,
+      this.addY(5)
+    );
     this.drawLine();
-    this.doc.text('Antrag für Auslagen bzw. Einsatzvergütung', 20, this.addY(5));
+    this.doc.text(
+      'Antrag für Auslagen bzw. Einsatzvergütung',
+      20,
+      this.addY(5)
+    );
 
     this.doc.setFont('helvetica', 'bold');
     this.doc.text('Kostenberechnung', 20, this.addY(5));
 
     this.doc.setFont('helvetica', 'normal');
-    this.doc.text(`${PdfGeneratorService.buildHoursRepresentation(metadata.startDate)} bis ${PdfGeneratorService.buildHoursRepresentation(metadata.endDate)}    rund ${PdfGeneratorService.calculateHalfHoursCount(metadata.startDate, metadata.endDate)} x 0,5 Stunden`, 20, this.addY(5));
+    this.doc.text(
+      `${PdfGeneratorService.buildHoursRepresentation(
+        metadata.startDate
+      )} bis ${PdfGeneratorService.buildHoursRepresentation(
+        metadata.endDate
+      )}    rund ${PdfGeneratorService.calculateHalfHoursCount(
+        metadata.startDate,
+        metadata.endDate
+      )} x 0,5 Stunden`,
+      20,
+      this.addY(5)
+    );
     this.drawLine();
     this.addEmptyLine();
 
     // generate list
     let totalCosts: number = 0;
 
+    crewsSelections.forEach((selection) => {
+      const halfHoursCount = PdfGeneratorService.calculateHalfHoursCount(
+        selection.startDate,
+        selection.endDate
+      );
+      this.incLineCountAndAddPageIfNeeded(2);
+      this.createCrewSelectionHeader(this.doc, selection, halfHoursCount);
+      this.addEmptyLine();
 
-    crewsSelections.forEach(selection => {
-        const halfHoursCount = PdfGeneratorService.calculateHalfHoursCount(selection.startDate, selection.endDate);
-        this.incLineCountAndAddPageIfNeeded(2)
-        this.createCrewSelectionHeader(this.doc, selection, halfHoursCount);
-        this.addEmptyLine();
-
-
-        selection.crew.forEach((member, index) => {
-          this.incLineCountAndAddPageIfNeeded(1)
-          const memberCosts = PdfGeneratorService.calculateCrwMemberCostRate(member, costRate, halfHoursCount);
-          totalCosts += memberCosts;
-          this.doc.text(`${member.surname} ${member.name}`, 20, this.addY(5));
-          this.doc.text('=', 60, this.addY(0));
-          this.doc.text(`${memberCosts} €`, 110, this.addY(0));
-        });
-        this.incLineCountAndAddPageIfNeeded(1)
-        this.addEmptyLine();
-      }
-    );
+      selection.crew.forEach((member, index) => {
+        this.incLineCountAndAddPageIfNeeded(1);
+        const memberCosts = PdfGeneratorService.calculateCrwMemberCostRate(
+          member,
+          costRate,
+          halfHoursCount
+        );
+        totalCosts += memberCosts;
+        this.doc.text(`${member.surname} ${member.name}`, 20, this.addY(5));
+        this.doc.text('=', 60, this.addY(0));
+        this.doc.text(`${memberCosts} €`, 110, this.addY(0));
+      });
+      this.incLineCountAndAddPageIfNeeded(1);
+      this.addEmptyLine();
+    });
 
     // total costs
-    this.incLineCountAndAddPageIfNeeded(1)
+    this.incLineCountAndAddPageIfNeeded(1);
     this.drawLine();
-    this.incLineCountAndAddPageIfNeeded(1)
+    this.incLineCountAndAddPageIfNeeded(1);
     this.doc.text('Gesamtkosten', 20, this.addY(5));
-    this.incLineCountAndAddPageIfNeeded(1)
+    this.incLineCountAndAddPageIfNeeded(1);
     this.doc.text('=', 60, this.addY(0));
-    this.incLineCountAndAddPageIfNeeded(1)
+    this.incLineCountAndAddPageIfNeeded(1);
     this.doc.text(`${totalCosts} €`, 110, this.addY(0));
-    this.incLineCountAndAddPageIfNeeded(1)
+    this.incLineCountAndAddPageIfNeeded(1);
     this.addEmptyLine();
-
 
     // footer
     // line 1
-    this.incLineCountAndAddPageIfNeeded(1)
+    this.incLineCountAndAddPageIfNeeded(1);
     this.doc.setFont('helvetica', 'bold');
     this.doc.text('Auslagen gemäß § 1 FwES:', 20, this.addY(5));
     this.doc.text(`Rechnung an:`, 110, this.addY(0));
     this.doc.setFont('helvetica', 'normal');
 
     // line 2
-    this.doc.text(`Schutterwald, den: ${PdfGeneratorService.buildFullDate(new Date())}`, 20, this.addY(5));
-    metadata.hasInvoiceReceiver ? this.doc.text(`${metadata.invoiceReceiverName}`, 110, this.addY(0)) : '';
+    this.doc.text(
+      `Schutterwald, den: ${PdfGeneratorService.buildFullDate(new Date())}`,
+      20,
+      this.addY(5)
+    );
+    metadata.hasInvoiceReceiver
+      ? this.doc.text(`${metadata.invoiceReceiverName}`, 110, this.addY(0))
+      : '';
 
     // line 3
     this.doc.text(`Stantke Thomas, Kdt`, 20, this.addY(5));
-    metadata.hasInvoiceReceiver ? this.doc.text(`${metadata.invoiceReceiverAddress}`, 110, this.addY(0)) : '';
+    metadata.hasInvoiceReceiver
+      ? this.doc.text(`${metadata.invoiceReceiverAddress}`, 110, this.addY(0))
+      : '';
 
     // line 3
-    metadata.hasInvoiceReceiver ? this.doc.text(`${metadata.invoiceReceiverPostcodeAndCity}`, 110, this.addY(5)) : '';
+    metadata.hasInvoiceReceiver
+      ? this.doc.text(
+        `${metadata.invoiceReceiverPostcodeAndCity}`,
+        110,
+        this.addY(5)
+      )
+      : '';
 
     this.doc.save(`${fileName}.pdf`);
   }
 
   incLineCountAndAddPageIfNeeded(inc: number): void {
     if (this.pageState.count > 35) {
-      this.doc.addPage()
-      this.doc.setPage(this.pageState.page + 1)
-      this.pdfY = defaultYStartPosition
-      this.pageState.page = this.pageState.page + 1
-      this.pageState.count = 0
-      return
+      this.doc.addPage();
+      this.doc.setPage(this.pageState.page + 1);
+      this.pdfY = defaultYStartPosition;
+      this.pageState.page = this.pageState.page + 1;
+      this.pageState.count = 0;
+      return;
     }
-    this.pageState.count = this.pageState.count + inc
+    this.pageState.count = this.pageState.count + inc;
   }
 
-  private createCrewSelectionHeader(doc: jsPDF, selection: CrewSelectorResponse, halfHoursCount: number): void {
+  private createCrewSelectionHeader(
+    doc: jsPDF,
+    selection: CrewSelectorResponse,
+    halfHoursCount: number
+  ): void {
     let dirtAllowance: boolean = false;
-    selection.crew.forEach(value => {
+    selection.crew.forEach((value) => {
       if (value.dirtAllowance) {
         dirtAllowance = true;
       }
     });
 
     if (dirtAllowance) {
-      this.doc.text(`${halfHoursCount} x 0,5 Stunden á 6 € + einmalige 2,00 € Schmutzzulage Pauschale`, 20, this.addY(5));
+      this.doc.text(
+        `${halfHoursCount} x 0,5 Stunden á 6 € + einmalige 2,00 € Schmutzzulage Pauschale`,
+        20,
+        this.addY(5)
+      );
       return;
     }
-    this.doc.text(`${halfHoursCount} x 0,5 Stunden á 6 €`, 20, this.addY(5));
+    this.doc.text(`${halfHoursCount} x 0,5 Stunden á 6,50 €`, 20, this.addY(5));
   }
 
   private addY(val: number): number {
@@ -208,4 +270,3 @@ export class PdfGeneratorService {
     this.doc.line(20, this.addY(2), 160, this.pdfY); // horizontal line
   }
 }
-
